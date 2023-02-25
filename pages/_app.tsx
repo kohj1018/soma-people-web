ee9d2ced75f4ce4b04632dfaffac341a230d31ee
kohj1018 <kohj1018@hanyang.ac.kr>
2023-02-26 08:12:09 +0900

    플러터 웹뷰 브릿지 함수 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import { SessionProvider } from 'next-auth/react'
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import GoogleAnalytics from '../components/GoogleAnalytics'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,6 +17,27 @@ const queryClient = new QueryClient({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  // Flutter Bridge Function 세팅
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    window.changePage = (index: number) => {
+      switch (index) {
+        case 0:
+          router.push('/')
+          break
+        case 1:
+          router.push('/board')
+          break
+        default:
+          router.push('/')
+          break
+      }
+    }
+  }, [])
+  
   return (
     <>
       <GoogleAnalytics />
