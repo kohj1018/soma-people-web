fbe499ee8ab6a260dd377d7cfaa67dd140621a6e
kohj1018 <kohj1018@hanyang.ac.kr>
2023-02-26 09:00:54 +0900

    [fix] Layout.tsx -> MainContainer.tsx 로 뷰포트 수정

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import BottomCenterSnackbar from '../common/BottomCenterSnackbar'
 import { useSnackbarOpenStore } from '../../stores/stores'
 
@@ -11,17 +10,11 @@ function Layout({ children }: Props) {
   const { message, isSnackbarOpen, setIsSnackbarOpen } = useSnackbarOpenStore()
 
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-
-      <div className='relative min-h-screen h-full min-w-[300px] w-full bg-white'>
-        {children}
-        {/*<MobileBottomNavigationBar router={router} selectedTap={selectedTap} setSelectedTap={setSelectedTap} />*/}
-        <BottomCenterSnackbar isSnackbarOpen={isSnackbarOpen} setIsSnackbarOpen={setIsSnackbarOpen} message={message} />
-      </div>
-    </>
+    <div className='relative min-h-screen h-full min-w-[300px] w-full bg-white'>
+      {children}
+      {/*<MobileBottomNavigationBar router={router} selectedTap={selectedTap} setSelectedTap={setSelectedTap} />*/}
+      <BottomCenterSnackbar isSnackbarOpen={isSnackbarOpen} setIsSnackbarOpen={setIsSnackbarOpen} message={message} />
+    </div>
   )
 }
 
--- components/layout/MainContainer.tsx
+++ components/layout/MainContainer.tsx
@@ -1,6 +1,7 @@
 import MainHeader from './MainHeader'
 import MainFooter from './MainFooter'
 import React from 'react'
+import Head from 'next/head'
 
 interface Props {
   children: React.ReactNode
@@ -9,6 +10,10 @@ interface Props {
 const MainContainer = ({ children }: Props) => {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
       <MainHeader />
       {children}
       <MainFooter />
