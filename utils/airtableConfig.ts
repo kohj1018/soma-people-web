a2ebb0b30232d8cebe461d98cb94ffb424ab9042
kohj1018 <kohj1018@hanyang.ac.kr>
2023-03-01 05:31:35 +0900

    [add] AirTable 인증 등록 함수 추가

--- utils/airtableConfig.ts
+++ utils/airtableConfig.ts
@@ -5,16 +5,33 @@ const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('app7WLkz
 
 export type CsType = '문의' | '건의' | '신고'
 export type ReportType = '유저' | '게시글' | '댓글' | 'NotReport'
-export const postCSContent = (csType: CsType, userId: number, nickname: string = 'nickname', content: string, reportTargetType: ReportType = 'NotReport', reportTargetId: number = 0) => base('customer-service').create([
+export const postCSContent = (csType: CsType, userId: number, name: string = 'name', content: string, reportTargetType: ReportType = 'NotReport', reportTargetId: number = 0) => base('customer-service').create([
     {
         "fields": {
             "csType": csType,
             "userId": parseInt(userId.toString()),
-            "nickname": nickname,
+            "name": name,
             "content": content,
             "createdAt": dayjs().format('YY-MM-DD HH:mm:ss'),
             "reportTargetId": parseInt(reportTargetId.toString()),
             "reportTargetType": reportTargetType
+        }
+    }
+])
+
+// @ts-ignore
+export const sendVerificationRequest = (userId: number, name: string, userType: UserType, cardinalNum: number | null, imgUrl: string) => base('certification').create([
+    {
+        "fields": {
+            "userId": userId,
+            "name": name,
+            "userType": userType,
+            "cardinalNum": cardinalNum ?? 0,
+            "photo": [
+                {
+                    "url": imgUrl
+                }
+            ]
         }
     }
 ])
