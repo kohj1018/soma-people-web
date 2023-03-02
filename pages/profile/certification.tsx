94fa42927f874e7f218dd99c73bf0eb724c352ab
kohj1018 <kohj1018@hanyang.ac.kr>
2023-03-02 23:57:48 +0900

    [fix/add] 소마인 인증 최종 구현 (Cloudinary로 변경)

--- pages/profile/certification.tsx
+++ pages/profile/certification.tsx
@@ -4,21 +4,21 @@ import MainArea from '../../components/layout/MainArea'
 import MobileCancelHeader from '../../components/layout/mobileHeader/MobileCancelHeader'
 import Image from 'next/image'
 import somaCertification from '../../public/somaCertification.png'
-import useDrivePicker from 'react-google-drive-picker'
 import { useEffect, useState } from 'react'
 import { sendCertificationRequest } from '../../utils/airtableConfig'
 import useUserInfo from '../../hooks/useUserInfo'
 import { useRouter } from 'next/router'
 import { useSnackbarOpenStore } from '../../stores/stores'
 import { useCertificationRequestStore } from '../../stores/localStorageStore/stores'
+import dayjs from 'dayjs'
 
 const Certification: NextPage = () => {
   const router = useRouter()
   const userInfo = useUserInfo()
   const { isAlreadyRequest, setIsAlreadyRequest } = useCertificationRequestStore()
-  const [openPicker, authResponse] = useDrivePicker()
-  const [uploadFileName, setUploadFileName] = useState<string>('')
-  const [uploadFileUrl, setUploadFileUrl] = useState<string>('')
+
+  const [image, setImage] = useState<File | null>(null)
+  const [uploadImageUrl, setUploadImageUrl] = useState<string>('')
   const { setMessage } = useSnackbarOpenStore()
 
   // 이미 인증받았거나 인증 요청한 상태이면 Redirect
@@ -34,39 +34,45 @@ const Certification: NextPage = () => {
     }
   }, [userInfo])
 
-  const handleOpenPicker = () => {
-    openPicker({
-      clientId: process.env.GOOGLE_DRIVE_CLIENT_ID as string,
-      developerKey: process.env.GOOGLE_DRIVE_API_KEY as string,
-      viewId: 'DOCS_IMAGES',
-      token: process.env.GOOGLE_DRIVE_ACCESS_TOKEN as string,
-      showUploadView: true,
-      showUploadFolders: false,
-      disableDefaultView: true,
-      multiselect: false,
-      setParentFolder: process.env.GOOGLE_DRIVE_TARGET_FOLDER_ID,
-      locale: 'ko',
-      callbackFunction: (data) => {
-        if (data.action === 'cancel') {
-          setUploadFileName('')
+  const uploadImage = () => {
+    if (!!image) {
+      if (uploadImageUrl) {
+        setMessage('이미 업로드 되었습니다. 상단의 등록버튼을 눌러 완료해주세요.')
+      } else {
+        const data = new FormData()
+        data.append('file', image)
+        data.append('upload_preset', process.env.CLOUDINARY_UPLOAD_PRESET as string)
+        data.append('cloud_name', process.env.CLOUDINARY_CLOUD_NAME as string)
+        if (!!userInfo) {
+          data.append('public_id', userInfo.userId + '_' + userInfo.name + '_' + (userInfo.cardinalNum ? `${userInfo.cardinalNum}기` : '') + '_' + userInfo.userType + '_' + dayjs().format('YY-MM-DD_HH:mm'))
         }
-        if (data.action === 'picked') {
-          setUploadFileName(data.docs[0].name)
-          setUploadFileUrl(data.docs[0].url)
-        }
-      },
-    })
+
+        fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`, {
+          method: 'post',
+          body: data
+        }).then((res) => res.json())
+          .then((data) => {
+            setUploadImageUrl(data.url)
+            setMessage('업로드 성공! 등록 버튼을 눌러주세요.')
+          })
+          .catch((err) => {
+            setMessage('업로드 중 에러가 발생했습니다.')
+          })
+      }
+    } else {
+      setMessage('파일을 먼저 선택해주세요.')
+    }
   }
 
   const handleSubmit = () => {
     if (!!userInfo) {
-      if (uploadFileUrl) {
-        sendCertificationRequest(userInfo?.userId, userInfo?.name, userInfo?.userType, userInfo?.cardinalNum, uploadFileUrl)
+      if (uploadImageUrl) {
+        sendCertificationRequest(userInfo?.userId, userInfo?.name, userInfo?.userType, userInfo?.cardinalNum, uploadImageUrl)
         setIsAlreadyRequest(true)
         setMessage('인증 신청이 완료되었습니다.')
         router.back()
       } else {
-        setMessage('캡처 화면을 먼저 업로드 해주세요.')
+        setMessage('사진 업로드 버튼을 눌러주세요.')
       }
     }
   }
@@ -93,16 +99,20 @@ const Certification: NextPage = () => {
         </div>
 
         <div className='mt-8 w-full px-5 flex items-center justify-between space-x-3'>
+          <input
+            type='file'
+            onChange={(e) => { if (e.target.files) setImage(e.target.files[0]) }}
+          />
           <button
-            onClick={handleOpenPicker}
+            onClick={uploadImage}
             className='px-3 py-2 rounded bg-slate-900'
           >
             <p className='text-base font-semibold text-white whitespace-nowrap'>사진 업로드</p>
           </button>
-          {uploadFileName &&
-            <p className='w-full px-3 py-2.5 rounded bg-gray-100 text-sm font-medium text-gray-400 truncate'>{uploadFileName}</p>
-          }
         </div>
+        {uploadImageUrl &&
+          <p className='mt-2 px-5 text-right text-sm font-medium text-green-400'>업로드가 완료되었습니다!</p>
+        }
       </MainArea>
     </MainContainer>
   )
