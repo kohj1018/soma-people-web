2e66074c4492126bd1fc0d98eb395871cacebd56
kohj1018 <kohj1018@hanyang.ac.kr>
2023-03-11 20:45:03 +0900

    [fix] 실명 입력

--- pages/auth/signUp.tsx
+++ pages/auth/signUp.tsx
@@ -31,8 +31,8 @@ const SignUp = () => {
   // 이름 입력 제한 함수
   const nameInputRestriction = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length < e.target.maxLength + 1) {
-      // if (!(/[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g).test(e.target.value)) {  // 한글만 입력되게 제한
-      if (!(/[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi).test(e.target.value)) {  // 특수문자 제한
+      if (!(/[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g).test(e.target.value)) {  // 한글만 입력되게 제한
+      // if (!(/[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi).test(e.target.value)) {  // 특수문자 제한
         setName(e.target.value)
       }
     }
@@ -99,11 +99,13 @@ const SignUp = () => {
           <section className='space-y-5'>
             <div>
               <article className='w-full flex items-center justify-between space-x-4'>
-                <p className='w-[2.625rem] text-base font-medium text-gray-900 whitespace-nowrap'>닉네임</p>
+                {/*<p className='w-[2.625rem] text-base font-medium text-gray-900 whitespace-nowrap'>닉네임</p>*/}
+                <p className='text-base font-medium text-gray-900 whitespace-nowrap'>이름</p>
                 <input
                   type='text'
                   className='w-full px-4 py-2 bg-gray-50 rounded text-base font-medium text-gray-500 placeholder:text-gray-300 focus:outline-none'
-                  placeholder='사용할 닉네임을 입력해주세요.'
+                  // placeholder='사용할 닉네임을 입력해주세요.'
+                  placeholder='실명을 입력해주세요.'
                   maxLength={20}
                   value={name}
                   onChange={(e) => nameInputRestriction(e)} // 모바일 환경에서는 maxLength 속성이 먹히지 않기 때문에 js 추가
@@ -115,13 +117,15 @@ const SignUp = () => {
               {/*}*/}
             </div>
             <article className='w-full flex items-center justify-between space-x-4'>
-              <p className='w-[2.625rem] text-base font-medium text-gray-900'>유형</p>
+              {/*<p className='w-[2.625rem] text-base font-medium text-gray-900'>유형</p>*/}
+              <p className='text-base font-medium text-gray-900'>유형</p>
               <MuiSelectUserType userType={userType} setUserType={setUserType} />
             </article>
             {(userType === '연수생' || userType === '멘토') &&
               <>
                 <article className='w-full flex items-center justify-between space-x-4'>
-                  <p className='w-[2.625rem] text-base font-medium text-gray-900 whitespace-nowrap'>기수</p>
+                  {/*<p className='w-[2.625rem] text-base font-medium text-gray-900 whitespace-nowrap'>기수</p>*/}
+                  <p className='text-base font-medium text-gray-900 whitespace-nowrap'>기수</p>
                   <input
                     type='number'
                     className='w-full px-4 py-2 bg-gray-50 rounded text-base font-medium text-gray-500 placeholder:text-gray-300 focus:outline-none'
