2311afacee40e5175f6205036e19ddd268cd8173
kohj1018 <kohj1018@hanyang.ac.kr>
2023-03-27 04:21:21 +0900

    [fix] isMobile 함수 수정

--- utils/functions/isMobile.ts
+++ utils/functions/isMobile.ts
@@ -1,3 +1,4 @@
 export function isMobile(): boolean {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  return navigator.userAgent === 'Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.88 Mobile Safari/537.36' || navigator.userAgent === 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1'
 }
