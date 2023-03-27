2d27d29dc875f052b460e29051c2a2ea125ba292
kohj1018 <kohj1018@hanyang.ac.kr>
2023-03-28 05:54:54 +0900

    [fix] firebaseToken 등록 안되는 문제 해결4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,18 +48,19 @@ export default function App({ Component, pageProps }: AppProps) {
     }
 
     // @ts-ignore
-    window.registerFirebaseToken = (firebaseInfoJSON: string) => {  // firebaseToken 등록함수
-      const firebaseInfo: FirebaseTokenInfo = JSON.parse(firebaseInfoJSON)
-      if (!!firebaseInfo.userId && !!firebaseInfo.firebaseToken) {
-        const today: string = dayjs().format('YYYY-MM-DD')
-        if (updatedAt === null || (!!updatedAt && dayjs(updatedAt).isAfter(today, 'month'))) { // 등록한 적 없거나, 마지막 수정 날짜보다 한 달 이상 됐을 때 업데이트
-          registerFirebaseToken(firebaseInfo.userId, { firebaseToken: firebaseInfo.firebaseToken })
-            .then(() => {
-              if (firebaseInfo.firebaseToken) setFirebaseToken(firebaseInfo.firebaseToken)
-              setUpdatedAt(today)
-            })
-        }
+    window.registerFirebaseToken = (userId: number, firebaseToken: string | null) => {  // firebaseToken 등록함수
+      // const firebaseInfo: FirebaseTokenInfo = JSON.parse(firebaseInfoJSON)
+      // if (!!firebaseInfo.userId && !!firebaseInfo.firebaseToken) {
+      //   const today: string = dayjs().format('YYYY-MM-DD')
+        // if (updatedAt === null || (!!updatedAt && dayjs(updatedAt).isAfter(today, 'month'))) { // 등록한 적 없거나, 마지막 수정 날짜보다 한 달 이상 됐을 때 업데이트
+      if (firebaseToken) {
+        registerFirebaseToken(userId, { firebaseToken: firebaseToken })
+          .then(() => {
+            setFirebaseToken(firebaseToken)
+            // setUpdatedAt(today)
+          })
       }
+        // }
     }
 
     // @ts-ignore
