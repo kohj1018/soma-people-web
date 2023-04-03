1b2cb28b0967aad3122db5b5b4c4c075f1db1747
kohj1018 <kohj1018@hanyang.ac.kr>
2023-04-03 17:54:04 +0900

    [fix] 유저 타입 태그 수정

--- components/tag/UserTypeTag.tsx
+++ components/tag/UserTypeTag.tsx
@@ -5,7 +5,7 @@ import School from '@mui/icons-material/School'
 import LocalLibrary from '@mui/icons-material/LocalLibrary'
 import LocalPolice from '@mui/icons-material/LocalPolice'
 import AccountBalance from '@mui/icons-material/AccountBalance'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
   userType: UserType
@@ -31,35 +31,61 @@ function UserTypeTag({ userType, cardinalNum, isAnonymous }: Props) {
     }
   }, [])
 
-  return (
-    <span className='px-1.5 py-1 inline-flex items-center space-x-0.5 rounded bg-gray-100'>
-      <UserTypeIcon userType={userType} cardinalNum={cardinalNum} />
-      <p className='text-xs font-semibold text-gray-600 lg:text-sm'>{processedUserType}</p>
-    </span>
-  )
+  switch (userType) {
+    case '연수생':
+      if (cardinalNum === THIS_YEAR_CARDINAL_NUM) {
+        return (
+          <CustomUserTypeTag textColor='text-blue-500' bgColor='bg-blue-50' processedUserType={processedUserType}>
+            <LocalFireDepartment className='!w-4 !h-4 text-blue-500 lg:!w-5 lg:!h-5' />
+          </CustomUserTypeTag>
+        )
+      } else {
+        return (
+          <CustomUserTypeTag textColor='text-indigo-500' bgColor='bg-indigo-50' processedUserType={processedUserType}>
+            <School className='!w-4 !h-4 text-indigo-500 lg:!w-5 lg:!h-5' />
+          </CustomUserTypeTag>
+        )
+      }
+    case '준비생':
+      return (
+        <CustomUserTypeTag textColor='text-emerald-500' bgColor='bg-emerald-50' processedUserType={processedUserType}>
+          <LocalLibrary className='!w-4 !h-4 text-emerald-500 lg:!w-5 lg:!h-5' />
+        </CustomUserTypeTag>
+      )
+    case '멘토':
+      return (
+        <CustomUserTypeTag textColor='text-purple-500' bgColor='bg-purple-50' processedUserType={processedUserType}>
+          <LocalPolice className='!w-4 !h-4 text-purple-500 lg:!w-5 lg:!h-5' />
+        </CustomUserTypeTag>
+      )
+    case '사무국':
+      return (
+        <CustomUserTypeTag textColor='text-pink-500' bgColor='bg-pink-50' processedUserType={processedUserType}>
+          <AccountBalance className='!w-4 !h-4 text-pink-500 lg:!w-5 lg:!h-5' />
+        </CustomUserTypeTag>
+      )
+    default:
+      return (
+        <CustomUserTypeTag textColor='text-gray-200' bgColor='bg-gray-50' processedUserType={processedUserType}>
+          <></>
+        </CustomUserTypeTag>
+      )
+  }
 }
 
 export default UserTypeTag
 
-interface UserTypeIconProps {
-  userType: UserType
-  cardinalNum: number | null
+interface CustomUserTypeTagProps {
+  children: React.ReactNode
+  textColor: string
+  bgColor: string
+  processedUserType: string
 }
-function UserTypeIcon({ userType, cardinalNum }: UserTypeIconProps) {
-  switch (userType) {
-    case '연수생':
-      if (cardinalNum === THIS_YEAR_CARDINAL_NUM) {
-        return <LocalFireDepartment className='!w-4 !h-4 text-blue-500 lg:!w-5 lg:!h-5' />
-      } else {
-        return <School className='!w-4 !h-4 text-blue-500 lg:!w-5 lg:!h-5' />
-      }
-    case '준비생':
-      return <LocalLibrary className='!w-4 !h-4 text-blue-500 lg:!w-5 lg:!h-5' />
-    case '멘토':
-      return <LocalPolice className='!w-4 !h-4 text-blue-500 lg:!w-5 lg:!h-5' />
-    case '사무국':
-      return <AccountBalance className='!w-4 !h-4 text-blue-500 lg:!w-5 lg:!h-5' />
-    default:
-      return <></>
-  }
+function CustomUserTypeTag({ children, textColor, bgColor, processedUserType }: CustomUserTypeTagProps) {
+  return (
+    <span className={'px-1.5 py-1 inline-flex items-center space-x-0.5 rounded ' + bgColor}>
+      {children}
+      <p className={'text-xs font-semibold text-gray-600 lg:text-sm ' + textColor}>{processedUserType}</p>
+    </span>
+  )
 }
