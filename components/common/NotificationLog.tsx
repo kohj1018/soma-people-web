daffa9f2225b6ef0812a4dde9cde9c65518f54a2
kohj1018 <kohj1018@hanyang.ac.kr>
2023-04-04 23:18:32 +0900

    [fix] 알림 기록 컴포넌트 퍼블리싱 수정

--- components/common/NotificationLog.tsx
+++ components/common/NotificationLog.tsx
@@ -4,6 +4,7 @@ import { NotificationType } from '../../utils/types/notificationType'
 import Campaign from '@mui/icons-material/Campaign'
 import { getElapsedTime } from '../../utils/functions/getElapsedTime'
 import dayjs from 'dayjs'
+import { checkNotificationLog } from '../../utils/apis/notificationLogsApi'
 
 interface Props {
   notificationLogInfo: NotificationLogInfoType
@@ -36,10 +37,16 @@ function CustomNotificationLog({ notificationLogInfo, bgColor }: CustomNotificat
   return (
     <Link
       href={`/post/${notificationLogInfo.postId}`}
-      className={'px-6 py-4 ' + bgColor}
+      className={'block px-6 py-4 rounded-lg ' + bgColor}
+      onClick={() => checkNotificationLog(notificationLogInfo.notificationLogId)}
     >
       <BoardNameTag boardName={notificationLogInfo.boardName} notificationType={notificationLogInfo.notificationType} />
-      <p className='mt-2 text-base font-medium text-gray-900'>{notificationLogInfo.content}</p>
+      <p className='mt-2 text-base font-medium text-gray-900 ellipsisTwoLine'>
+        {notificationLogInfo.notificationType === '공지사항'
+          ? notificationLogInfo.content
+          : `새로운 ${notificationLogInfo.notificationType}이 달렸어요 : ${notificationLogInfo.content}`
+        }
+      </p>
       <p className='mt-4 text-sm font-semibold text-gray-500'>{getElapsedTime(dayjs(notificationLogInfo.createdAt))}</p>
     </Link>
   )
@@ -52,16 +59,16 @@ interface BoardNameTagProps {
 function BoardNameTag({ boardName, notificationType }: BoardNameTagProps) {
   if (notificationType === '공지사항') {
     return (
-      <span className='px-1.5 py-1 flex items-center space-x-1 bg-red-100'>
+      <div className='inline-block px-1.5 py-1 flex items-center space-x-1 rounded bg-red-100'>
         <Campaign className='!w-4 !h-4 text-red-500' />
         <p className='text-sm font-semibold text-red-500'>{boardName}</p>
-      </span>
+      </div>
     )
   } else {
     return (
-      <span className='px-1.5 py-1 bg-blue-100'>
+      <div className='inline-block px-1.5 py-1 rounded bg-blue-100'>
         <p className='text-sm font-semibold text-blue-500'>{boardName}</p>
-      </span>
+      </div>
     )
   }
 }
