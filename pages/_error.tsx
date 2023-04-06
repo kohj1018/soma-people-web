e292780e1fcc19c119021ef4355b944e7af00fda
kohj1018 <kohj1018@hanyang.ac.kr>
2023-04-06 19:02:31 +0900

    [add] 에러페이지 세팅

---
+++ pages/_error.tsx
@@ -0,0 +1,24 @@
+import { NextPage, NextPageContext } from 'next'
+
+interface Props {
+  statusCode?: number
+}
+
+const Error: NextPage = ({ statusCode }: Props) => {
+
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : "An error occurred on client"}
+    </p>
+  )
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
+
